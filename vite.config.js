e9b3d0c9595bcb1f0173de53f5f9e1dad7ec008b
Ilya <ilieturcan424@gmail.com>
2026-04-13 17:05:22 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
+                catalog: resolve(__dirname, "catalog.html"),
             },
         },
     },
