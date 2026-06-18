80cbc67478279b1df1cf120ff37a2e1a4c23bd0e
Ilya <ilieturcan424@gmail.com>
2026-06-18 13:03:20 +0300

    final commit

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,50 @@
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
+import viteImagemin from "vite-plugin-imagemin";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-    plugins: [
-            tailwindcss(),
-        ],
     build: {
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
-                catalog: resolve(__dirname, "catalog.html"),
+                catalog: "catalog.html",
+                blog: "blog.html",
+                about: "about.html",
             },
         },
     },
+    plugins: [
+        tailwindcss(),
+        viteImagemin({
+            gifsicle: {
+                optimizationLevel: 7,
+                interlaced: false,
+            },
+            optipng: {
+                optimizationLevel: 7,
+            },
+            mozjpeg: {
+                quality: 70,
+            },
+            pngquant: {
+                quality: [0.8, 0.9],
+                speed: 4,
+            },
+            svgo: {
+                plugins: [
+                    {
+                        name: "removeViewBox",
+                    },
+                    {
+                        name: "removeEmptyAttrs",
+                        active: false,
+                    },
+                ],
+            },
+        }),
+    ],
 });
